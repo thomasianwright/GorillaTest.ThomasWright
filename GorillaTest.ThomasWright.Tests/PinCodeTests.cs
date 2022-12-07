8e257dba8ea4b3f898332007c521997b4b3bc48e
Tom <Thomas.Wright@caliqon.com>
2022-12-07 10:23:51 +0000

    Initial

---
+++ GorillaTest.ThomasWright.Tests/PinCodeTests.cs
@@ -0,0 +1,30 @@
+namespace GorillaTest.ThomasWright.Tests;
+
+public class PinCodeTests
+{
+    private readonly string PinSuccessMessage;
+    private readonly string PinFailureMessage;
+
+    public PinCodeTests()
+    {
+        PinSuccessMessage = "Pin is valid";
+        PinFailureMessage = "Pin is invalid";
+    }
+    
+    [Theory]
+    [InlineData("1234")]
+    [InlineData("4312")]
+    public void Assert_Test_Success_With_Correctly_Formatted_Pincodes(string value)
+    {
+        Assert.Equal(PinCode.ParsePinCode(value), PinSuccessMessage);
+    }
+
+    [Theory]
+    [InlineData("f3d2")]
+    [InlineData("42g43")]
+    [InlineData("f31342")]
+    public void Assert_Test_Failure_With_Incorrectly_Formatted_Pincodes(string value)
+    {
+        Assert.Equal(PinCode.ParsePinCode(value), PinFailureMessage);
+    }
+}
---
+++ GorillaTest.ThomasWright/PinCode.cs
@@ -0,0 +1,14 @@
+﻿namespace GorillaTest.ThomasWright;
+
+public static class PinCode
+{
+    public static string ParsePinCode(string pin)
+    {
+        if (pin.Length is 4 or 6 && int.TryParse(pin, out var _))
+        {
+            return "Pin is valid";
+        }
+
+        return "Pin is invalid";
+    } 
+}
